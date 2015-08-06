dc29b38afb7d218f71fb92c39d4173e0ee90d8a6
Matt Wette <matthew.wette@verizon.net>
2015-08-06 16:38:52 -0700

    maybe working now

--- test-suite/nyacc/calc.scm
+++ test-suite/nyacc/calc.scm
@@ -7,29 +7,29 @@
 ;; notice and this notice are preserved.  This file is offered as-is,
 ;; without any warranty.
 
+(add-to-load-path (string-append (getcwd) "/../../module"))
+
 (use-modules (nyacc lalr))
 (use-modules (nyacc lex))
 
 (define calc-spec
   (lalr-spec
-   (precedence< (left "+" "-") (left "*" "/"))
+   (prec< (left "+" "-") (left "*" "/"))
    (start expr)
    (grammar
     (expr
      (expr "+" expr ($$ (+ $1 $3)))
-     ;;(expr "-" expr ($$ (- $1 $3)))
-     ;;(expr "*" expr ($$ (* $1 $3)))
-     ;;(expr "/" expr ($$ (/ $1 $3)))
+     (expr "-" expr ($$ (- $1 $3)))
+     (expr "*" expr ($$ (* $1 $3)))
+     (expr "/" expr ($$ (/ $1 $3)))
      ('$fx ($$ (string->number $1))))
     )))
-
-(pp-lalr-grammar calc-spec)
-;;(pp-lalr-machine calc-mach)
-
-#|
 (define calc-mach (make-lalr-machine calc-spec))
 ;;(define calc-mach (compact-machine calc-mach))
 (define calc-mach (hashify-machine calc-mach))
+
+;;(pp-lalr-grammar calc-spec)
+;;(pp-lalr-machine calc-mach)
 
 (define parse-expr
   (let ((gen-lexer (make-lexer-generator (assq-ref calc-mach 'mtab)))
@@ -38,6 +38,5 @@
 
 (define res (with-input-from-string "1 + 4 / 2 * 3 - 5" parse-expr))
 (simple-format #t "expect 2; get ~S\n" res) ;; expect: 2
-|#
 
 ;; --- last line
