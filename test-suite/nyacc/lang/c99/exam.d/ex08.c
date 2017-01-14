513a3865f9bfddd11917ddfa075cc7ee309f976b
Matt Wette <matt.wette@gmail.com>
2017-01-14 13:30:47 -0800

    still debugginug

--- test-suite/nyacc/lang/c99/exam.d/ex08.c
+++ test-suite/nyacc/lang/c99/exam.d/ex08.c
@@ -1,6 +1,12 @@
-#define assert(x) ((x) ? (void)0 : assert_fail (#x))
+#define foo1(X, Y) (X+Y)
+int x = foo1(1,2);
+/*
+#define foo(A, B, ...)  foo1(A,B)+bar(__FILE__, A, B, __VA_ARGS__)
 
-int foo() {
-  assert(boo);
-}
+int x = foo(1, 2, 3, 4, foo1(2,3));
+#define fix1(x) #x
+char *abc1 = fix1(bar);
 
+#define fix2(X) _ ## X
+int fix(abc2) = 1;
+*/
