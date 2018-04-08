6c43dc238746ae312859b56c9c1ef9adcd789776
Matt Wette <mwette@alumni.caltech.edu>
2018-04-07 18:42:10 -0700

    mainloop as backgroud task

--- examples/nyacc/lang/c99/ffi-exam/dbus04.scm
+++ examples/nyacc/lang/c99/ffi-exam/dbus04.scm
@@ -2,6 +2,10 @@
 ;; https://lists.freedesktop.org/archives/dbus/2007-October/008859.html
 ;; https://stackoverflow.com/questions/9378593/dbuswatch-and-dbustimeout-examples
 
+(define-module (dbus04)
+  #:export (start-dbus-session-loop)
+  )
+  
 (use-modules (ice-9 pretty-print))
 (use-modules (ice-9 format))
 (define (sf fmt . args) (apply simple-format #t fmt args))
@@ -19,8 +23,6 @@
 (use-modules (ffi epoll))
 (use-modules (ffi dbus))
 (use-modules (dbus00))
-
-(define d-a (@@ (bytestructures guile struct) debug-alignment))
 
 ;; ====================================
 ;; use case 01: want to queue in any thread, poll for ready
@@ -56,39 +58,36 @@
 	 dbus-flags))
    0 '(1 4 8 16)))
 
-(define-record-type dbus-data
-  (make-dbus-data fd ev wv)
-  dbus-data?
-  (fd dbus-data-fd)			; epoll fd, if in epoll set
-  (ev dbus-data-ev)			; epoll event
-  (wv dbus-data-wv set-dbus-data-wv!)	; watch vector
-  )
-
-(define dbus-data-maxw 3)
-
 (define (scm->addr scm)
   (ffi:pointer-address (ffi:scm->pointer scm)))
 (define (addr->scm addr)
   (ffi:pointer->scm (ffi:make-pointer addr)))
 
+(define-record-type dbus-data
+  (make-dbus-data fd ev wv)
+  dbus-data?
+  (fd dbus-data-fd)			; epoll fd, if in epoll set
+  (ev dbus-data-ev)			; epoll event
+  (wv dbus-data-wv set-dbus-data-wv!))	; watch vector
+
+(define *dbus-maxw* 3)
 (define *dbus-fd-dict* (make-hash-table 31))
 
-;; (define (dbus-lookup-fd goes here 
 ;; if not found add a blank entry
 (define (dbus-lookup-fd fd)
   (or (hashv-ref *dbus-fd-dict* fd)
       (let* ((event (make-struct-epoll_event))
-	     (ddent (make-dbus-data fd event (make-vector dbus-data-maxw #f))))
+	     (ddent (make-dbus-data fd event (make-vector *dbus-maxw* #f))))
 	(hashv-set! *dbus-fd-dict* fd ddent)
 	(fh-object-set! event 'data 'ptr (scm->addr ddent))
-	(ff "new ddent@ 0x~x   w/ event ~s\n" (scm->addr ddent) event)
+	;;(ff "new ddent@ 0x~x   w/ event ~s\n" (scm->addr ddent) event)
 	ddent)))
 
 (define (find-wv-slot wv)
-  (let iter ((i 0))
+  (let iter ((i 0) (n (vector-length wv)))
     (cond
-     ((= i dbus-data-maxw) -1)
-     ((vector-ref wv i) (iter (1+ i)))
+     ((= i n) -1)
+     ((vector-ref wv i) (iter (1+ i) n))
      (else i))))
 
 (define (dbus-data-watched? ddent)
@@ -103,15 +102,13 @@
 (define (dbus-data-free ddent)
   (if #f #f))
 
+;; no need implement this with use of the get_dispatch_status/dispatch loop
 (define (dispatch-status connection ~status data)
-  (display "dispatch-status called\n")
-  (let ((status (wrap-DBusDispatchStatus ~status))
-	)
-    (if (eq? status 'DBUS_DISPATCH_DATA_REMAINS)
-	#f)
-    #f))
+  ;;(display "dispatch-status called\n")
+  (if #f #f))
 
-;; (define (add-watch ...) goes here
+;; @deffn {Procedure} add-watch watch data
+;; @end deffn
 (define (add-watch ~watch data)
   (let* ((watch (make-DBusWatch* ~watch))
 	 (muxfd (ffi:pointer-address data))
@@ -133,15 +130,15 @@
     (if (dbus-data-watchless? ddent)
 	(epoll_ctl muxfd (EPOLL '_CTL_ADD) addfd (pointer-to event)))
       
-    (sf "    ~S" (bytestructure-bytevector (fh-object-val event))) ;; add \n !
     ;; Set watches based on flags.
     (let* ((wv (dbus-data-wv ddent)) (wx (find-wv-slot wv)))
-      (sf " @ wx=~S\n" wx)
       (if (negative? wx) (error "max exceeded")
 	  (vector-set! wv wx watch)))
     
     TRUE))
 
+;; @deffn {Procedure} remove-watch watch data
+;; @end deffn
 (define (remove-watch ~watch data)
   (let* ((watch (make-DBusWatch* ~watch))
 	 (muxfd (ffi:pointer-address data))
@@ -160,6 +157,8 @@
     ;;    (epoll_ctl muxfd (EPOLL '_CTL_DEL) delfd (ffi:scm->pointer ddent)))
     (if #f #f)))
 
+;; @deffn {Procedure} watch-toggled watch data
+;; @end deffn
 (define (watch-toggled ~watch data)
   (let* ((watch (make-DBusWatch* ~watch))
 	 (muxfd (ffi:pointer-address data))
@@ -194,11 +193,14 @@
 		 struct-epoll_event-vec #f)
 
 (define (filter-func c m data)
-  (sf "filter-func called ...\n  iface: ~S\n member: ~S\n   path: ~S\n"
-      (ffi:pointer->string (dbus_message_get_interface m))
-      (ffi:pointer->string (dbus_message_get_member m))
-      (ffi:pointer->string (dbus_message_get_path m)))
-  (DBUS 'HANDLER_RESULT_HANDLED))
+  (when #f
+    (sf "filter-func called ...\n  iface : ~S\n  member: ~S\n  path  : ~S\n"
+	(ffi:pointer->string (dbus_message_get_interface m))
+	(ffi:pointer->string (dbus_message_get_member m))
+	(ffi:pointer->string (dbus_message_get_path m))))
+  (DBUS 'HANDLER_RESULT_NOT_YET_HANDLED)
+  ;;(DBUS 'HANDLER_RESULT_HANDLED)
+  )
 
 (define (my-main-loop connection)
   (let* ((muxfd (epoll_create 1))
@@ -220,30 +222,27 @@
     (dbus_bus_add_match connection "type='method_call'" NULL)
     ;;
     (let loop ()
-      (display "waiting ...\n")
+      ;;(display "waiting ...\n")
       (let iter ((i 0) (n (epoll_wait muxfd eventp max-events -1)))
 	(unless (= i n)
-	  (sf "\n  i=~S ...\n" i)
+	  ;;(sf "\n  i=~S ...\n" i)
 	  (let* ((event (fh-object-ref eventv i))
 		 (events (bytestructure-ref event 'events))
 		 (data-ptr (bytestructure-ref event 'data 'ptr))
 		 (ddent (addr->scm data-ptr))
 		 (flags (epoll-events->dbus-watch-flags events)))
-	    ;;(ff "a  event = ~s\n" (bytestructure-bytevector event))
-	    (ff "  events=0x~x => flags=0x~x\n" events flags)
-	    ;;(sf "  ddent=~S\n" ddent)
-	    ;;(sf "  watches=~S\n" watches)
 	    (vector-for-each
 	     (lambda (ix watch)
-	       (when (and watch
-			  (nonzero? (logand flags (dbus_watch_get_flags watch))))
-		 (while (equal? FALSE (dbus_watch_handle watch flags)) (sleep 1))
-		 (dbus_connection_ref connection)
-		 (while (eq? 'DBUS_DISPATCH_DATA_REMAINS
-			     (dbus_connection_get_dispatch_status connection))
-		   (sf "    dispatch ...\n")
-		   (dbus_connection_dispatch connection))
-		 (dbus_connection_unref connection)))
+	       (when watch
+		 (let ((flags (logand flags (dbus_watch_get_flags watch))))
+		   (while (equal? FALSE (dbus_watch_handle watch flags))
+		     (sleep 1))
+		   (dbus_connection_ref connection)
+		   (while (eq? 'DBUS_DISPATCH_DATA_REMAINS
+			       (dbus_connection_get_dispatch_status connection))
+		     ;;(sf "    dispatch ...\n")
+		     (dbus_connection_dispatch connection))
+		   (dbus_connection_unref connection))))
 	     (dbus-data-wv ddent)))
 	  (iter (1+ i) n)))
       (loop))
@@ -252,14 +251,26 @@
 
 ;; === main ==========================
 
-(define error (make-DBusError))
-(dbus_error_init (pointer-to error))
+;;(define error (make-DBusError))
+;;(dbus_error_init (pointer-to error))
+;;(define conn (dbus_bus_get 'DBUS_BUS_SESSION (pointer-to error)))
+;;(check-error error)
+;;(sf "conn=~S\n" (ffi:pointer->string (dbus_bus_get_unique_name conn)))
+;;(my-main-loop conn) ;; later in a dedicated thread
 
-(define conn (dbus_bus_get 'DBUS_BUS_SESSION (pointer-to error)))
-(check-error error)
-(sf "conn=~S\n" (ffi:pointer->string (dbus_bus_get_unique_name conn)))
-
-(my-main-loop conn) ;; later in a dedicated thread
-(sleep 3)
+(define (start-dbus-session-loop)
+  (let* ((error
+	  (let ((error (make-DBusError)))
+	    (dbus_error_init (pointer-to error))
+	    error))
+	 (conn
+	  (let ((conn (dbus_bus_get 'DBUS_BUS_SESSION (pointer-to error))))
+	    (check-error error)
+	    (sf "conn: ~S\n"
+		(ffi:pointer->string (dbus_bus_get_unique_name conn)))
+	    conn))
+	 )
+    (call-with-new-thread (lambda () (my-main-loop conn)))
+    conn))
 
 ;; --- last line ---
--- examples/nyacc/lang/c99/ffi-exam/dbus05.scm
+++ examples/nyacc/lang/c99/ffi-exam/dbus05.scm
@@ -1,6 +1,14 @@
 ;; dbus05.scm - dbus04 applied to example
 
-(load "dbus04.scm")
+(use-modules (dbus00))
+(use-modules (dbus04))
+(use-modules (ffi dbus))
+(use-modules (system ffi-help-rt))
+(use-modules ((system foreign) #:prefix ffi:))
+(use-modules (ice-9 pretty-print))
+
+(define (sf fmt . args) (apply simple-format #t fmt args))
+(define pp pretty-print)
 
 (define msg (dbus_message_new_method_call
 	     "org.freedesktop.DBus"		; bus name (was NULL)
@@ -8,27 +16,24 @@
 	     "org.freedesktop.DBus.Debug.Stats"	; interface name
 	     "GetStats"))			; method
 
-(define pending (make-DBusPendingCall*))
+(define (send-msg conn msg)
+  (let ((pending (make-DBusPendingCall*)))
+    (or (dbus_connection_send_with_reply conn msg (pointer-to pending) -1)
+	(error "*** send_with_reply FAILED\n"))
+    ;;(dbus_message_unref msg)
+    pending))
 
-(or (dbus_connection_send_with_reply conn msg (pointer-to pending) -1)
-    (error "*** send_with_reply FAILED\n"))
-(if (zero? (fh-object-ref pending)) (display "*** pending NULL\n"))
+(define (handle-it pending)
+  (dbus_pending_call_block pending)
+  (let ((msg (dbus_pending_call_steal_reply pending))
+	(msg-iter (make-DBusMessageIter))
+	)
+    (if (zero? (fh-object-ref msg)) (error "*** reply message NULL\n"))
+    (dbus_pending_call_unref pending)
+    (dbus_message_iter_init msg (pointer-to msg-iter))
+    (sf "result:\n")
+    (pretty-print (read-dbus-val (pointer-to msg-iter)) #:per-line-prefix "  ")
+    (dbus_message_unref msg)))
 
-(dbus_message_unref msg)
-(dbus_pending_call_block pending)
-
-(set! msg (dbus_pending_call_steal_reply pending))
-(if (zero? (fh-object-ref msg)) (error "*** reply message NULL\n"))
-(sf "msg from reply:~S, serial:~S\n" msg (dbus_message_get_serial msg))
-
-(define msg-iter (make-DBusMessageIter))
-(dbus_pending_call_unref pending)
-
-(sf "iter_init => ~S\n" (dbus_message_iter_init msg (pointer-to msg-iter)))
-(sf "result:\n")
-(pretty-print (read-dbus-val (pointer-to msg-iter)) #:per-line-prefix "  ")
-
-(dbus_message_unref msg)
-;;(dbus_connection_close conn)
-
+;; (define conn (start-dbus-session-loop))
 ;; --- last line ---
