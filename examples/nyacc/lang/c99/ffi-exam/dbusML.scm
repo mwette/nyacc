f5ca0eeb213598477709768459d27f560a6bd590
Matt Wette <mwette@alumni.caltech.edu>
2018-04-14 08:09:05 -0700

    added copyright/license

--- examples/nyacc/lang/c99/ffi-exam/dbusML.scm
+++ examples/nyacc/lang/c99/ffi-exam/dbusML.scm
@@ -1,6 +1,21 @@
-;; dbus04.scm - dbus in another thread : sync calls
+;; dbusML.scm - dbus in another thread : sync calls
 ;; https://lists.freedesktop.org/archives/dbus/2007-October/008859.html
 ;; https://stackoverflow.com/questions/9378593/dbuswatch-and-dbustimeout-examples
+
+;; Copyright (C) 2018 Matthew R. Wette
+;;
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with this library; if not, see <http://www.gnu.org/licenses/>
 
 (define-module (dbusML)
   #:export (spawn-dbus-mainloop)
