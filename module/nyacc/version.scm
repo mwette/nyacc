1d28f5dbdd42f6caaa2cb9f140814479e8c00fd6
Matt Wette <mwette@alumni.caltech.edu>
2018-11-19 12:32:05 -0800

    new release 0.86.6

--- module/nyacc/version.scm
+++ module/nyacc/version.scm
@@ -20,6 +20,6 @@
 (define-module (nyacc version)
   #:export (*nyacc-version*))
 
-(define *nyacc-version* "0.86.4")
+(define *nyacc-version* "0.86.6")
 
 ;; --- last line ---
--- module/system/ffi-help-rt.scm
+++ module/system/ffi-help-rt.scm
@@ -57,7 +57,7 @@
   #:use-module ((system foreign) #:prefix ffi:)
   #:use-module (srfi srfi-9))
 
-(define *ffi-help-version* "0.86.4")
+(define *ffi-help-version* "0.86.6")
 
 (define (sferr fmt . args) (apply simple-format (current-error-port) fmt args))
 
