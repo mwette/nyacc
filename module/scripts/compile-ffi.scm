c52fdb10fc723392643b7e32128f18e8f4f5194a
Matt Wette <mwette@alumni.caltech.edu>
2020-02-22 07:04:15 -0800

    new release V1.01.1

--- module/scripts/compile-ffi.scm
+++ module/scripts/compile-ffi.scm
@@ -30,9 +30,9 @@
   #:use-module ((system base compile) #:select (compile-file))
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-37)
-  #:version (1 01 0))
+  #:version (1 01 1))
 
-(define *ffi-help-version* "1.01.0")
+(define *ffi-help-version* "1.01.1")
 
 (define %summary
   "Compile a ffi-file (C interface spec) to Scheme (or maybe .go).")
