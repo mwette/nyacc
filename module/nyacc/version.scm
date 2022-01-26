ac5cbe64f1a58010e7eb56849d2ad55a87fc333f
Matt Wette <mwette@alumni.caltech.edu>
2022-01-26 08:48:15 -0800

    sync

--- module/nyacc/version.scm
+++ module/nyacc/version.scm
@@ -19,8 +19,8 @@
 
 (define-module (nyacc version)
   #:export (*nyacc-version*)
-  #:version (1 06 5))
+  #:version (1 06 7))
 
-(define *nyacc-version* "1.06.5")
+(define *nyacc-version* "1.06.7")
 
 ;; --- last line ---
