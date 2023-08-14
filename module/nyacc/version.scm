7f9676863220aefff0403d3e593f7594e9c35ca7
Matt Wette <mwette@alumni.caltech.edu>
2023-08-14 13:29:08 -0700

    new release 1.08.4

--- module/nyacc/version.scm
+++ module/nyacc/version.scm
@@ -19,8 +19,8 @@
 
 (define-module (nyacc version)
   #:export (*nyacc-version*)
-  #:version (1 08 3))
+  #:version (1 08 4))
 
-(define *nyacc-version* "1.08.3")
+(define *nyacc-version* "1.08.4")
 
 ;; --- last line ---
