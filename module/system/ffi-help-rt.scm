a88fa2498b1026a33e0c97746735740a057f6fbf
Matt Wette <mwette@alumni.caltech.edu>
2023-10-18 18:13:42 -0700

    new release 1.09.1

--- module/system/ffi-help-rt.scm
+++ module/system/ffi-help-rt.scm
@@ -59,9 +59,9 @@
   #:use-module (rnrs bytevectors)
   #:use-module ((system foreign) #:prefix ffi:)
   #:use-module (srfi srfi-9)
-  #:version (1 09 0))
+  #:version (1 09 1))
 
-(define *ffi-help-version* "1.09.0")
+(define *ffi-help-version* "1.09.1")
 
 (use-modules (ice-9 pretty-print))
 (define (sferr fmt . args)
