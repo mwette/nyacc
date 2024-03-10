adc12d16446b8faf0c856dcb75d664ebcb4211d3
Matthew Wette <mwette@alumni.caltech.edu>
2024-03-10 06:45:35 -0700

    start gkinter project

--- examples/nyacc/lang/ffi-help/gkinter.scm
+++ examples/nyacc/lang/ffi-help/gkinter.scm
@@ -1,66 +1,131 @@
+(define-module (gkinter)
+  #:export (gkinter-init
+            wm
+            .
+            vwait-forever
+            )
+  #:declarative? #f
+  )
+(use-modules (system ffi-help-rt))
+(use-modules (bytestructures guile))
+
+(use-modules (ffi glib))
+(use-modules (ffi gobject))
+(use-modules (ffi gtk2))
+
+(define wind (make-parameter (make-hash-table)))
+(define revd (make-parameter (make-hash-table)))
+(define (wind-set! win val)
+  (unless (symbol? win) (error "expecting symbolic name"))
+  (hashq-set! wind win val)
+  (hashq-set! revd val win))
+(define (wind-ref win) (hashq-ref wind win)) ;; => gtk window
+(define (revd-ref val) (hashq-ref revd val)) ;; => symbol
+
+(define (gkinter-init)
+  (let ((argc (bytestructure int 0)))
+    (gtk_init (pointer-to argc) NULL)
+    (wind-set '. (gtk_window_new 'GTK_wINDOw_TOPLEVEL))
+    (if #f #f)))
+
+(define (vwait-forever)
+  (gtk_main))
+
+;; should routines return symbolic name or
+
+(define* (wm wn #:key title)
+  (unless (symbol? wn) (error "expecting symbolic name"))
+  (let ((wv (wind-ref w)))
+    (when title (gtk_window_set_title wv title))
+    wn))
+
+;; @deffn {Procedure} grid [options]
+;; column (int)
+;; row (int)
+;; sticky (string)
+;; @end deffn
+(define* (grid wn #:key column row sticky)
+  wn)
+
+(define* (grid-columnconfigure wn col #:key weight)
+  wn)
+
+(define* (grid-rowconfigure wn col #:key weight)
+  wn)
+
+(eval-when (expand load eval)
+  (define (window-parent win)
+    (let* (;;(sym (syntax->datum win))
+           (sym win)
+           (str (symbol->string sym))
+           (rix (string-rindex str #\.)))
+      (if (zero? rix) '.) (string->symbol (substring str 0 rix)))))
+
+(define-syntax tkdef
+  (syntax-rules ()
+    ((_ name (maker args ...))
+     (let ((wid (maker args ...)))
+       (module-add! (current-module) wn #,wid)
+       wid))))
+
+;; @deffn {Syntax} ttk:entry w #:key width textvariable
+;; @end deffn
+
+;; @deffn {Syntax} ttk:label w #:key text
+;; @end deffn
+(define* (make-label #:key text)
+  (let ((wid (gtk_label_new)))
+    (if text (gtk_label_set_text wid text))
+    wid))
+(define-syntax ttk:label
+  (syntax-rules ()
+    ((_ wn args ...)
+     (tkdef wn (make-label args ...)))))
+
+
+;; @deffn {Syntax} ttk:button w #:key text
+;; @end deffn
+(define* (make-button #:key text command)
+  (let ((wid (gtk_button_new)))
+    (if text (gtk_button_set_label wid text))
+    (if command
+        (let ((cb (fh-cast GCallback command)))
+          (g_signal_connect wid "clicked" cb NULL)))
+    wid))
+(define-syntax-rule (ttk:button wn args ...)
+  (tkdef wn (make-button args ...)))
+
+(define* (grid-columnconfigure w col #:key weight)
+  #t)
+
+(define* (grid-rowconfigure w row #:key weight)
+  #t)
+
+(define* (grid w #:key column row sticky)
+  )
+
+
 #|
-package require Tk
+(define (main)
+  (define window #f)
+  (define button #f)
+  (define argc (bytestructure int 0))
 
-wm title . "Feet to Meters"
-grid [ttk::frame .c -padding "3 3 12 12"] -column 0 -row 0 -sticky nwes
-grid columnconfigure . 0 -weight 1; grid rowconfigure . 0 -weight 1
+  (gtk_init (pointer-to argc) NULL)
 
-grid [ttk::entry .c.feet -width 7 -textvariable feet] -column 2 -row 1 -sticky we
-grid [ttk::label .c.meters -textvariable meters] -column 2 -row 2 -sticky we
-grid [ttk::button .c.calc -text "Calculate" -command calculate] -column 3 -row 3 -sticky w
+  (set! window (gtk_window_new 'GTK_WINDOW_TOPLEVEL))
+  (g_signal_connect window "delete-event" delete-event NULL)
+  (g_signal_connect window "destroy" ~gtk_main_quit NULL)
+  (gtk_container_set_border_width window 10)
 
-grid [ttk::label .c.flbl -text "feet"] -column 3 -row 1 -sticky w
-grid [ttk::label .c.islbl -text "is equivalent to"] -column 1 -row 2 -sticky e
-grid [ttk::label .c.mlbl -text "meters"] -column 3 -row 2 -sticky w
+  (set! button (gtk_button_new_with_label "Hello World"))
+  (g_signal_connect button "clicked" (fh-cast GCallback hello) NULL)
+  (g_signal_connect_swapped button "clicked" ~gtk_widget_destroy window)
+  (gtk_container_add window button)
 
-foreach w [winfo children .c] {grid configure $w -padx 5 -pady 5}
-focus .c.feet
-bind . <Return> {calculate}
+  (gtk_widget_show button)
+  (gtk_widget_show window)
 
-proc calculate {} {
-   if {[catch {
-       set ::meters [expr {round($::feet*0.3048*10000.0)/10000.0}]
-   }]!=0} {
-       set ::meters ""
-   }
-}
-
-vwait forever
+  (gtk_main))
 |#
-
-(use-modules (gkinter))
-
-(wm #:title . "Feet to Meters")
-(grid (ttk:frame .c -padding #(3 3 12 12)) #:column 0 #:row 0 #:sticky 'nwes)
-(grid columnconfigure . 0 #:weight 1)
-(grid rowconfigure . 0 #:weight 1)
-
-(grid (ttk:entry .c.feet #:width 7 #:textvariable feet)
-      #:column 2 #:row 1 #:sticky we)
-(grid (ttk:label .c.meters #:textvariable meters)
-      #:column 2 #:row 2 #:sticky we)
-(grid (ttk:button .c.calc #:text "Calculate" #:command calculate)
-      #:column 3 #:row 3 #:sticky w)
-
-(grid (ttk:label .c.flbl #:text "feet")
-      #:column 3 #:row 1 #:sticky w)
-(grid (ttk:label .c.islbl #:text "is equivalent to")
-      #:column 1 #:row 2 #:sticky e)
-(grid (ttk:label .c.mlbl #:text "meters")
-      #:column 3 #:row 2 #:sticky w)
-
-(for-each
- (lambda (w) (grid configure $w #:padx 5 #:pady 5))
- (winfo children .c))
-(focus .c.feet)
-(bind . <Return> {calculate})
-
-(define (calculate)
-  (try
-   (set! ::meters (expr {round($::feet*0.3048*10000.0)/10000.0})
-   }]!=0} {
-       set ::meters ""
-   }
-}
-
-(vwait forever)
+;; --- last line ---
