10dbab91716b0a5b3c08deb351bff58e146d1c6e
Matthew Wette <mwette@alumni.caltech.edu>
2024-07-19 06:39:18 -0700

    {cpp,parser}.scm: update copyright date

--- module/nyacc/lang/c99/cpp.scm
+++ module/nyacc/lang/c99/cpp.scm
@@ -1,6 +1,6 @@
 ;;; lang/c/cpp.scm - C preprocessor
 
-;; Copyright (C) 2015-2023 Matthew Wette
+;; Copyright (C) 2015-2024 Matthew Wette
 ;;
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
--- module/nyacc/lang/c99/parser.scm
+++ module/nyacc/lang/c99/parser.scm
@@ -1,6 +1,6 @@
 ;;; nyacc/lang/c99/parser.scm - C parser execution
 
-;; Copyright (C) 2015-2023 Matthew Wette
+;; Copyright (C) 2015-2024 Matthew Wette
 ;;
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
