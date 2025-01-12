aca84a7b9c24ad9cf0374ae2f1009b544b9d12db
Matthew Wette <mwette@alumni.caltech.edu>
2025-01-12 09:57:42 -0800

    use .dir-locals.el, using 1 for and=>

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;; Per-directory local variables for GNU Emacs 23 and later.
+
+((nil             . ((fill-column . 78)
+                     (tab-width   .  8))
+ (scheme-mode
+  . ((indent-tabs-mode . nil)
+     (eval . (put 'and=>               'scheme-indent-function 1))
+     (eval . (put 'pass-if             'scheme-indent-function 1))))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)))
+ (texinfo-mode    . ((indent-tabs-mode . nil)
+                     (fill-column . 78)))))
