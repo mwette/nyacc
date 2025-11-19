0b8c8e7370aaacd3878f24ff1612813d515260eb
Matthew Wette <mwette@alumni.caltech.edu>
2025-11-19 05:54:29 -0800

    update copyright

--- module/nyacc/lang/c99/munge.scm
+++ module/nyacc/lang/c99/munge.scm
@@ -1,6 +1,6 @@
 ;;; nyacc/lang/c99/munge.scm - util's for processing output of the C99 parser
 
-;; Copyright (C) 2015-2022 Matthew R. Wette
+;; Copyright (C) 2015-2022,2025 Matthew Wette
 ;;
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
