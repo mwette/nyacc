efdc5167c0831e2294858571be2ae1fbf09af745
Matthew Wette <mwette@alumni.caltech.edu>
2026-01-05 11:58:49 -0800

    sync

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,8 @@
  (scheme-mode
   . ((indent-tabs-mode . nil)
      ;;(eval . (add-to-list 'write-file-functions 'delete-trailing-whitespace))
-     ;;(eval . (add-to-list 'write-file-functions 'untabify))
+     ;;(eval . (add-to-list 'write-file-functions
+;;			  (lambda () (untabify 0 (buffer-end)))))
      (eval . (put 'sx-match            'scheme-indent-function 1))
      (eval . (put 'sx-match-tail       'scheme-indent-function 1))
      (eval . (put 'with-arch           'scheme-indent-function 1))
