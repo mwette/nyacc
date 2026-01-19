9fbef8fa1d55656251f9acef86a73c0d238716bc
Matthew Wette <mwette@alumni.caltech.edu>
2026-01-19 14:07:36 -0800

    add cpp-defs for __has_include, __has_include_next

--- module/nyacc/lang/c99/util.scm
+++ module/nyacc/lang/c99/util.scm
@@ -139,7 +139,9 @@
   (let* ((cmd (string-append (resolve-CC CC) " -dM -E - </dev/null"))
          (ip (open-input-pipe cmd)))
     (let loop ((line (read-line ip 'trim)))
-      (if (eof-object? line) '()
+      (if (eof-object? line)
+          '("__has_include=__has_include__"
+            "__has_include_next=__has_include_next__")
           (cons (convert-line line) (loop (read-line ip 'trim)))))))
 
 
