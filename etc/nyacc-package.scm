6dee42aa10da35b2fb65bf1acb2b90595410f4ef
Matthew Wette <mwette@alumni.caltech.edu>
2026-02-20 12:31:50 -0800

    trying more general package

--- etc/nyacc-package.scm
+++ etc/nyacc-package.scm
@@ -15,7 +15,7 @@
 ;; You should have received a copy of the GNU Lesser General Public License
 ;; along with this library; if not, see <http://www.gnu.org/licenses/>
 
-(define-module (nyacc-guix)
+(define-module (nyacc-package)
   #:use-module (guix)
   #:use-module (guix packages)
   #:use-module (guix git)
@@ -25,33 +25,30 @@
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages guile))
 
-;; nyacc uses a subtle versioning scheme:
-;;   X.YY.Z is for releases (w/ tag VX.YY.Z)
-;;   X.YY.ZZ is for deveopent towards X.YY.0 (w/ tag dev-X.YY.ZZ)
 
 (define nyacc-sha256-base32-map
   '(("9.99.9" . "marker - do not delete")
-    ("3.03.03" . "0h9m3mszc2wrhvxacm9wphci5ip2g8cw3k6k677lx4mfrfys49xk")
     ("3.02.0" . "0lkjc52x7q04ris0vndn32cylfnzfrqqj2m3s8m0kbbj5rl2ggyh")
+    ("3.00.0" . "066wg0hvr7272bdxbn8b2ahn4yqrphfir8f9w9qd222s4ipd0iqa")
     ("0.00.0" . "marker - do not delete")))
 
 (define nyacc-base
   (package
    (name "nyacc-base")
-   (version "0.00.0")
+   (version "3.00.0")
    (source 
     (origin
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/mwette/nyacc")
-           (commit "V" version)))
+           (commit (string-append "V" version))))
      (file-name (string-append "nyacc-" version))
      (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))
    (build-system gnu-build-system)
    (native-inputs (list guile-3.0))
    (home-page "https://github.com/mwette/nyacc")
    (synopsis "parser generator, c parser, ffi-helper for guile")
-   (license (list lgpl3+))
+   (license lgpl3+)
    (description
     "NYACC, for Not Yet Another Compiler Compiler, is set of guile modules for
 generating parsers and lexical analyzers.  It also provides sample parsers
@@ -62,6 +59,28 @@ Guile Scheme bindings for C-based libraries.
 
 It provides (partially implemented) compilers based on above mentioned
 parsers to allow execution with Guile as extension languages.")))
+
+(define-public nyacc
+  (package
+   (inherit nyacc-base)
+   (name "nyacc")
+   (version "3.02.0")
+   (source 
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/mwette/nyacc")
+           (commit (string-append "V" version))))
+     (file-name (string-append "nyacc-" version))
+     (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))))
+
+
+;; -- development versions
+
+(define nyacc-dev-sha256-base32-map
+  '(("9.99.99" . "marker - do not delete")
+    ("3.03.03" . "0h9m3mszc2wrhvxacm9wphci5ip2g8cw3k6k677lx4mfrfys49xk")
+    ("0.00.00" . "marker - do not delete")))
 
 (define-public nyacc-dev
   (package
@@ -75,20 +94,23 @@ parsers to allow execution with Guile as extension languages.")))
            (url "https://github.com/mwette/nyacc")
            (commit (string-append "dev-" version))))
      (file-name (string-append "nyacc-dev-" version))
-     (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))))
+     (sha256 (base32 (assoc-ref nyacc-dev-sha256-base32-map version)))))))
 
-(define-public nyacc-3.02-github
+
+;; -- local package 
+
+(define vcs-file?
+  (or (git-predicate (dirname (current-source-directory)))) (const #t)))
+
+;not in a Git checkout
+(define-public nyacc-local
   (package
    (inherit nyacc-base)
    (name "nyacc")
-   (version "3.02.0")
-   (source 
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/mwette/nyacc")
-           (commit (string-append "V" version))))
-     (file-name (string-append "nyacc-" version))
-     (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))))
+   (version "local")
+   (source
+    (local-file ".." "nyacc-local" #:recursive? #t #:select? vcs-file?))))
+
+nyacc-local
 
 ;; --- last line ---
