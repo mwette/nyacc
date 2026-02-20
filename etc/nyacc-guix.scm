726affd0296c3b54a83bddbab02955ab7eaf1dd2
Matthew Wette <mwette@alumni.caltech.edu>
2026-02-20 08:42:52 -0800

    sync

---
+++ etc/nyacc-guix.scm
@@ -0,0 +1,90 @@
+;; nyacc/guix.scm
+
+;; Copyright (C) 2026 Matthew Wette
+;;
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with this library; if not, see <http://www.gnu.org/licenses/>
+
+(define-module (nyacc-guix)
+  #:use-module (guix)
+  #:use-module (guix packages)
+  #:use-module (guix git)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix licenses)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages guile))
+
+(define nyacc-sha256-base32-map
+  '(("9.99.9" . "marker - do not delete")
+    ("3.03.03" . "0h9m3mszc2wrhvxacm9wphci5ip2g8cw3k6k677lx4mfrfys49xk")
+    ("3.02.0" . "0lkjc52x7q04ris0vndn32cylfnzfrqqj2m3s8m0kbbj5rl2ggyh")
+    ("0.00.0" . "marker - do not delete")))
+
+(define nyacc-base
+  (package
+   (name "nyacc-base")
+   (version "0.00.0")
+   (source 
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/mwette/nyacc")
+           (commit "V" version)))
+     (file-name (string-append "nyacc-" version))
+     (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))
+   (build-system gnu-build-system)
+   (native-inputs (list guile-3.0))
+   (home-page "https://github.com/mwette/nyacc")
+   (synopsis "parser generator, c parser, ffi-helper for guile")
+   (license (list lgpl3+))
+   (description
+    "NYACC, for Not Yet Another Compiler Compiler, is set of guile modules for
+generating parsers and lexical analyzers.  It also provides sample parsers
+and pretty-printers using SXML trees as an intermediate representation.
+
+It provides a decent C parser and a `FFI Helper' tool to help create
+Guile Scheme bindings for C-based libraries.
+
+It provides (partially implemented) compilers based on above mentioned
+parsers to allow execution with Guile as extension languages.")))
+
+(define-public nyacc-dev
+  (package
+   (inherit nyacc-base)
+   (name "nyacc-dev")
+   (version "3.03.03")
+   (source 
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/mwette/nyacc")
+           (commit "dev-" version)))
+     (file-name (string-append "nyacc-dev-" version))
+     (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))))
+
+(define-public nyacc-3.02-github
+  (package
+   (inherit nyacc-base)
+   (name "nyacc")
+   (version "3.02.0")
+   (source 
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/mwette/nyacc")
+           (commit "dev-" version)))
+     (file-name (string-append "nyacc-" version))
+     (sha256 (base32 (assoc-ref nyacc-sha256-base32-map version)))))))
+
+;; --- last line ---
