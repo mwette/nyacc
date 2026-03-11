1017f450b923af2e9dd55334fce41d08224a8062
Matthew Wette <mwette@alumni.caltech.edu>
2026-03-11 12:50:32 -0700

    release V3.04.2

--- module/scripts/compile-ffi.scm
+++ module/scripts/compile-ffi.scm
@@ -31,9 +31,9 @@
   #:use-module (nyacc foreign arch-info)
   #:use-module ((nyacc lang c99 ffi-help)
                 #:select (compile-ffi-file *fh-backend*))
-  #:version (3 04 1))
+  #:version (3 04 2))
 
-(define *ffi-help-version* "3.04.1")
+(define *ffi-help-version* "3.04.2")
 
 (define (compile-scm file)
   (compile-file file
