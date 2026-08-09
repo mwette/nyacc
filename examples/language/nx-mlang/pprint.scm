5514889ec81e40e14540fd771701a58e342a4328
Matthew Wette <mwette@alumni.caltech.edu>
2026-08-09 08:31:20 -0700

    added local to comment: local is used for translation

--- examples/language/nx-mlang/pprint.scm
+++ examples/language/nx-mlang/pprint.scm
@@ -180,6 +180,9 @@
        (sf "~A(" name) (ppxin iputs) (sf ")\n")
        (push-il) (ppxin stmt-list) (pop-il) (sf "end\n"))
 
+      ((local (ident ,name))            ; added to il for processing
+       (sf "%: local ~A\n" name))
+
       ((comm ,text)
        (sf "%~A\n" text))
 
